a2605f51218dd5d44806f5e2100721328dadcb13
Ivan <zhurenko.ivan@mail.ru>
2021-09-28 21:39:14 +0300

    Добавил задание 11.1

---
+++ 11_AdvancedOOPFeatures/homework_11.1/src/main/java/EmpolyeeComparatorBySalary.java
@@ -0,0 +1,15 @@
+import java.util.Comparator;
+
+public class EmpolyeeComparatorBySalary implements Comparator<Employee> {
+    @Override
+    public int compare(Employee o1, Employee o2) {
+        if (o1.getSalary() < o2.getSalary()) {
+            return -1;
+        }else if (o1.getSalary().equals(o2.getSalary())) {
+            return 0;
+        }else return 1;
+    }
+}
+
+
+
--- 11_AdvancedOOPFeatures/homework_11.1/src/main/java/Main.java
+++ 11_AdvancedOOPFeatures/homework_11.1/src/main/java/Main.java
@@ -1,15 +1,28 @@
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
 
-    public static final String STAFF_TXT = "data/staff.txt";
+    public static final String STAFF_TXT = "C:\\ДЗ Java\\11_AdvancedOOPFeatures\\homework_11.1\\data\\staff.txt";
 
     public static void main(String[] args) {
         List<Employee> staff = Employee.loadStaffFromFile(STAFF_TXT);
         System.out.println(staff);
+        sortBySalaryAndAlphabet(staff);
+        System.out.println(staff);
     }
 
     public static void sortBySalaryAndAlphabet(List<Employee> staff) {
+//        Comparator<Employee> comparator = new EmpolyeeComparatorBySalary().thenComparing(new EmployeeComparatorByName());
+        Collections.sort(staff, (o1, o2) -> {
+            int i = o1.getSalary().compareTo(o2.getSalary());
+            if (i == 0) {
+                i = o1.getName().compareTo(o2.getName());
+                return i;
+            } else return i;
+        });
+
         //TODO Метод должен отсортировать сотрудников по заработной плате и алфавиту.
     }
 }
