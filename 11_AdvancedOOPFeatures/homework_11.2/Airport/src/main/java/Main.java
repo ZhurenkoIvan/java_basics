8627e0d322f8f594a4c31772f5ff4cbe5ff0fed9
zhure <zhurenko.ivan@mail.ru>
2021-09-30 10:49:58 +0300

    Добавил задание 11.2

--- 11_AdvancedOOPFeatures/homework_11.2/Airport/src/main/java/Main.java
+++ 11_AdvancedOOPFeatures/homework_11.2/Airport/src/main/java/Main.java
@@ -1,7 +1,10 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
-import java.util.Collections;
-import java.util.List;
+import com.skillbox.airport.Terminal;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,8 +13,20 @@ public class Main {
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
+        ArrayList<Flight> allPlanesLeavingInTheNextTwoHours = new ArrayList<>();
+        Date currentTime = new Date();
+        Date twoHoursLater = new Date(System.currentTimeMillis() + 7200000);
+        List<Terminal> allTerminals = airport.getTerminals();
+        for (Terminal terminal : allTerminals) {
+            ArrayList<Flight> stream = terminal.getFlights().stream()
+                    .filter(flight -> flight.getDate().after(currentTime) && flight.getDate().before(twoHoursLater))
+                    .filter(flight -> flight.getType().equals(Flight.Type.DEPARTURE))
+                    .collect(Collectors.toCollection(ArrayList::new));
+            allPlanesLeavingInTheNextTwoHours.addAll(stream);
+
+        }
         //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
+        return allPlanesLeavingInTheNextTwoHours;
     }
 
 }
--- 11_AdvancedOOPFeatures/homework_11.2/Employees/src/main/java/Main.java
+++ 11_AdvancedOOPFeatures/homework_11.2/Employees/src/main/java/Main.java
@@ -1,4 +1,5 @@
-import java.util.List;
+import java.util.*;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -11,8 +12,13 @@ public class Main {
     }
 
     public static Employee findEmployeeWithHighestSalary(List<Employee> staff, int year) {
+        Date afterDate = new Date(year - 1900, Calendar.JANUARY, 1);
+        Date beforeDate = new Date(year - 1899, Calendar.JANUARY, 1);
+        Optional<Employee> max = staff.stream()
+                .filter(employee -> employee.getWorkStart().after(afterDate) && employee.getWorkStart().before(beforeDate))
+                .max(Comparator.comparing(Employee::getSalary));
+        return max.orElse(null);
         //TODO Метод должен вернуть сотрудника с максимальной зарплатой среди тех,
         // кто пришёл в году, указанном в переменной year
-        return null;
     }
 }
