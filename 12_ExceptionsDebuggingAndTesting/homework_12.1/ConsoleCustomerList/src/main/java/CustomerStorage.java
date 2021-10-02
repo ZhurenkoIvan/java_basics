80a73d88cc2987299f7bc2f7a6066231cf11d670
Ivan <zhurenko.ivan@mail.ru>
2021-10-02 22:26:32 +0300

    Добавил задание 12.1

--- 12_ExceptionsDebuggingAndTesting/homework_12.1/ConsoleCustomerList/src/main/java/CustomerStorage.java
+++ 12_ExceptionsDebuggingAndTesting/homework_12.1/ConsoleCustomerList/src/main/java/CustomerStorage.java
@@ -3,20 +3,26 @@ import java.util.Map;
 
 public class CustomerStorage {
     private final Map<String, Customer> storage;
+    public final String EMAIL_REGEX = "[A-Za-z.]+[@][a-z]+[.][A-Za-z]+";
+    public final String FULL_NAME_REGEX = "[А-яёЁ]+[\\s][А-яёЁ]+";
+    public final String PHONE_NUMBER_REGEX = "[+][0-9]{11}";
+    public final String REGEX = FULL_NAME_REGEX + "[\\s]" + EMAIL_REGEX + "[\\s]" + PHONE_NUMBER_REGEX;
 
     public CustomerStorage() {
         storage = new HashMap<>();
     }
 
-    public void addCustomer(String data) {
+    public void addCustomer(String data){
+        if(!data.matches(REGEX)){
+            throw new IllegalArgumentException();
+        }
         final int INDEX_NAME = 0;
         final int INDEX_SURNAME = 1;
         final int INDEX_EMAIL = 2;
         final int INDEX_PHONE = 3;
-
-        String[] components = data.split("\\s+");
-        String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
-        storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
+            String[] components = data.split("\\s+");
+                String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
+                storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
     }
 
     public void listCustomers() {
--- 12_ExceptionsDebuggingAndTesting/homework_12.1/ConsoleCustomerList/src/main/java/Main.java
+++ 12_ExceptionsDebuggingAndTesting/homework_12.1/ConsoleCustomerList/src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
@@ -15,21 +16,26 @@ public class Main {
 
         while (true) {
             String command = scanner.nextLine();
-            String[] tokens = command.split("\\s+", 2);
+                String[] tokens = command.split("\\s+", 2);
 
-            if (tokens[0].equals("add")) {
-                executor.addCustomer(tokens[1]);
-            } else if (tokens[0].equals("list")) {
-                executor.listCustomers();
-            } else if (tokens[0].equals("remove")) {
-                executor.removeCustomer(tokens[1]);
-            } else if (tokens[0].equals("count")) {
-                System.out.println("There are " + executor.getCount() + " customers");
-            } else if (tokens[0].equals("help")) {
-                System.out.println(helpText);
-            } else {
-                System.out.println(COMMAND_ERROR);
-            }
+                if (tokens[0].equals("add")) {
+                    executor.addCustomer(tokens[1]);
+                } else if (tokens[0].equals("list")) {
+                    executor.listCustomers();
+                } else if (tokens[0].equals("remove")) {        //При подаче пустой строки вылетает исключение
+                    try {
+                        executor.removeCustomer(tokens[1]);
+                    } catch (IndexOutOfBoundsException e) {
+                        e.printStackTrace();
+                    }
+
+                } else if (tokens[0].equals("count")) {
+                    System.out.println("There are " + executor.getCount() + " customers");
+                } else if (tokens[0].equals("help")) {
+                    System.out.println(helpText);
+                } else {
+                    System.out.println(COMMAND_ERROR);
+                }
         }
     }
 }
