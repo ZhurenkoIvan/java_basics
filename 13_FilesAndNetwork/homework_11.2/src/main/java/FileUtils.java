535c1409a509b2e640bd092a866700ff6f7fb1b5
Ivan <zhurenko.ivan@mail.ru>
2021-10-06 19:56:21 +0300

    Добавил задание 13.2

--- 13_FilesAndNetwork/homework_11.2/src/main/java/FileUtils.java
+++ 13_FilesAndNetwork/homework_11.2/src/main/java/FileUtils.java
@@ -1,5 +1,78 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class FileUtils {
     public static void copyFolder(String sourceDirectory, String destinationDirectory) {
-        // TODO: write code copy content of sourceDirectory to destinationDirectory
+        List<File> allDirectories = new ArrayList<>();
+        List<File> allFiles = new ArrayList<>();
+
+        File sourceFile = new File(sourceDirectory);
+
+        allDirectories.add(sourceFile);
+        while (!allDirectories.isEmpty()) {
+            File file = allDirectories.get(0);
+            File[] files = file.listFiles();
+            for (File fileInDirectory : files) {
+                if (fileInDirectory.isFile()){
+                    allFiles.add(fileInDirectory);
+                    createFileInDestinationPath(fileInDirectory, sourceDirectory, destinationDirectory);
+                } else {
+                    allDirectories.add(fileInDirectory);
+                    createDirectoryInDestinationPath(fileInDirectory, sourceDirectory, destinationDirectory);
+                }
+            }
+            allDirectories.remove(file);
+        }
+        copyFiles(allFiles, sourceDirectory, destinationDirectory);
+    }
+
+
+    private static void createDirectoryInDestinationPath (File file, String sourceDirectory,String destinationDirectory) {
+        String filePath = file.getAbsolutePath();
+        int end = sourceDirectory.length();
+        String destinationFilePath = destinationDirectory + filePath.substring(end);
+        File destinationFile = new File(destinationFilePath);
+        destinationFile.mkdir();
+    }
+
+    private static void createFileInDestinationPath (File file, String sourceDirectory,String destinationDirectory) {
+        String filePath = file.getAbsolutePath();
+        int end = sourceDirectory.length();
+        String destinationFilePath = destinationDirectory + filePath.substring(end);
+        File destinationFile = new File(destinationFilePath);
+        try {
+            destinationFile.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void copyFiles (List<File> fileList, String sourceDirectory, String destinationDirectory) {
+        for (File fileSource : fileList) {
+            String sourceFilePath = fileSource.getAbsolutePath();
+            int end = sourceDirectory.length();
+            String destinationFilePath = destinationDirectory + sourceFilePath.substring(end);
+            File fileDest = new File(destinationFilePath);
+            InputStream is = null;
+            OutputStream os = null;
+            try {
+                is = new FileInputStream(fileSource);
+                os = new FileOutputStream(fileDest);
+                byte[] buffer = new byte[1024];
+                int length;
+                while ((length = is.read(buffer)) > 0) {
+                    os.write(buffer, 0, length);
+                }
+                is.close();
+                os.close();
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 }
--- 13_FilesAndNetwork/homework_11.2/src/main/java/Main.java
+++ 13_FilesAndNetwork/homework_11.2/src/main/java/Main.java
@@ -1,4 +1,21 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите папку, которую необходимо скопировать: ");
+        String sourceDirectory = scanner.nextLine();
+        System.out.println("Введите конечную папку: ");
+        String destinationDirectory = scanner.nextLine();
+        try {
+            FileUtils.copyFolder(sourceDirectory, destinationDirectory);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
+
+
 }
