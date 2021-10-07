9183e2519c8f7e3bd90862e29992397771c76c33
Ivan <zhurenko.ivan@mail.ru>
2021-10-07 22:19:44 +0300

    Добавил задание 13.1

--- 13_FilesAndNetwork/homework_11.1/src/main/java/FileUtils.java
+++ 13_FilesAndNetwork/homework_11.1/src/main/java/FileUtils.java
@@ -4,6 +4,27 @@ import java.util.List;
 
 public class FileUtils {
 
+    public static String folderSizeFormatter(Long folderSize, String path) {
+        double intermediateValue = folderSize;
+        if (folderSize < 1024) System.out.println("Размер папки " + path + "Составляет" + folderSize + "байт");
+        if (folderSize < 1_048_576 && folderSize >= 1024) {
+            folderSize = (long) (intermediateValue / 1024.0 * 10);
+            intermediateValue = folderSize / 10.0;
+            return  "Размер папки " + path + " Составляет " + intermediateValue + " Кб";
+        }
+        if (folderSize < 1_073_741_824L && folderSize >= 1_048_576) {
+            folderSize = (long) (intermediateValue / 1_048_576.0 * 10);
+            intermediateValue = folderSize / 10.0;
+            return "Размер папки " + path + " Составляет " + intermediateValue + " Мб";
+        }
+        else  {
+            folderSize = (long) (intermediateValue / 1_073_741_824.0 * 10);
+            intermediateValue = folderSize / 10.0;
+            return "Размер папки " + path + " Составляет " + intermediateValue + " Гб";
+        }
+
+    }
+
 
 
     public static long calculateFolderSize(String path) {
--- 13_FilesAndNetwork/homework_11.1/src/main/java/Main.java
+++ 13_FilesAndNetwork/homework_11.1/src/main/java/Main.java
@@ -5,24 +5,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String path = scanner.nextLine();
         try {
-            Long folderSize = FileUtils.calculateFolderSize(path);
-            double intermediateValue = folderSize;
-            if (folderSize < 1024) System.out.println("Размер папки " + path + "Составляет" + folderSize + "байт");
-            if (folderSize < 1_048_576 && folderSize >= 1024) {
-                folderSize = (long) (intermediateValue / 1024.0 * 10);
-                intermediateValue = folderSize / 10.0;
-                System.out.println("Размер папки " + path + " Составляет " + intermediateValue + " Кб");
-            }
-            if (folderSize < 1_073_741_824L && folderSize >= 1_048_576) {
-                folderSize = (long) (intermediateValue / 1_048_576.0 * 10);
-                intermediateValue = folderSize / 10.0;
-                System.out.println("Размер папки " + path + " Составляет " + intermediateValue + " Мб");
-            }
-            if (folderSize < 1_099_511_627_776L && folderSize >= 1_073_741_824L) {
-                folderSize = (long) (intermediateValue / 1_073_741_824.0 * 10);
-                intermediateValue = folderSize / 10.0;
-                System.out.println("Размер папки " + path + " Составляет " + intermediateValue + " Гб");
-            }
+            System.out.println(FileUtils.folderSizeFormatter(FileUtils.calculateFolderSize(path), path));
         }
         catch (IllegalArgumentException ex) {
             ex.printStackTrace();
