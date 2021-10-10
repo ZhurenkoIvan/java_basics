eca522b64e8fc1b0c60373da3aee9887dc066615
Ivan <zhurenko.ivan@mail.ru>
2021-10-10 16:21:44 +0300

    Подредактировал дз 13.4

--- 13_FilesAndNetwork/homework_13.4/src/main/java/Main.java
+++ 13_FilesAndNetwork/homework_13.4/src/main/java/Main.java
@@ -5,39 +5,26 @@ import org.jsoup.select.Elements;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
 import java.net.URL;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.regex.MatchResult;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Stream;
 
 
 public class Main {
     public static void main(String[] args) {
         try {
-            Document lentaHtml = Jsoup.connect("https://lenta.ru/").get();
-            Elements images = lentaHtml.select("img");
+            Document lentaHtml = Jsoup.connect("https://lenta.ru/").maxBodySize(0).get();
+            Elements images = lentaHtml.select("img.g-picture");
             images.forEach(image -> {
                 String absLink = image.attr("abs:src");
                 System.out.println(absLink);
-                String regex = "[.][a-z]{3,4}";
-                Pattern pattern = Pattern.compile(regex);
-                Matcher matcher = pattern.matcher(absLink);
-                AtomicInteger end = new AtomicInteger();
-                String name;
-                Stream<MatchResult> results = matcher.results();
-                results.map(result -> result.end())
-                    .forEach(result -> end.set(result));
-                name = absLink.substring(absLink.lastIndexOf("/"),end.intValue());
+                String name = absLink.substring(absLink.lastIndexOf("/"));
+
                 try {
                     URL url = new URL(absLink);
                     BufferedImage img = ImageIO.read(url);
                     File file = new File("src/Images/" + name);
                     file.createNewFile();
                     ImageIO.write(img, "jpg", file);
-                } catch (IOException e) {
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
 
@@ -45,7 +32,5 @@ public class Main {
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-
-
     }
 }
