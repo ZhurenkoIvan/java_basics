9eb1aaad1117f652efe2abc9d9d82fbf23c925ef
Ivan <zhurenko.ivan@mail.ru>
2021-10-13 23:40:23 +0300

    Отредактировал 14.1

--- 14_SQLAndHibernate/jdbc/src/main/java/Main.java
+++ 14_SQLAndHibernate/jdbc/src/main/java/Main.java
@@ -5,24 +5,21 @@ public class Main {
         String url = "jdbc:mysql://localhost:3306/skillbox";
         String user = "root";
         String pass = "Vitylin97";
-        try {
-            Connection connection = DriverManager.getConnection(url, user, pass);
-            Statement statement = connection.createStatement();
-            ResultSet coursesAverageBuy =  statement.executeQuery("SELECT pl.course_name, " +
-                    "(count(course_name) / (max(MONTH (pl.subscription_date)) + 1 - min(month(pl.subscription_date)))) AS average_buy" +
-                    " FROM purchaselist pl" +
-                    " GROUP BY course_name;");
-            while (coursesAverageBuy.next()) {
-                String courseName = coursesAverageBuy.getString("course_name");
-                double averageBuy = coursesAverageBuy.getDouble("average_buy");
-                System.out.println(courseName + " - " + averageBuy);
+        try (Connection connection = DriverManager.getConnection(url, user, pass)) {
+            String sql = "SELECT pl.course_name, (count(course_name) / (max(MONTH (pl.subscription_date)) + 1 - min(month(pl.subscription_date)))) AS average_buy FROM purchaselist pl WHERE YEAR(subscription_date) = (?) GROUP BY course_name;";
+            int year = 2018;
+            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                preparedStatement.setInt(1, year);
+                try (ResultSet coursesAverageBuy =  preparedStatement.executeQuery()) {
+                    while (coursesAverageBuy.next()) {
+                        String courseName = coursesAverageBuy.getString("course_name");
+                        double averageBuy = coursesAverageBuy.getDouble("average_buy");
+                        System.out.println(courseName + " - " + averageBuy);
+                    }
+                }
             }
-            coursesAverageBuy.close();
-            connection.close();
-            statement.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
         }
-
     }
 }
