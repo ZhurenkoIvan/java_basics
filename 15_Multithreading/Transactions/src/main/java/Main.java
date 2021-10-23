8d5ed0c812687840c62e1c4ba82a629bcdc5d9a0
Ivan <zhurenko.ivan@mail.ru>
2021-10-23 18:50:23 +0300

    скорректировал задание 15.2

--- 15_Multithreading/Transactions/src/main/java/Main.java
+++ 15_Multithreading/Transactions/src/main/java/Main.java
@@ -1,4 +1,6 @@
 import java.util.HashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 
 public class Main {
@@ -14,11 +16,21 @@ public class Main {
         bank.setAccounts(accounts);
 
         //Запускаю потоки
-        for (int i = 0; i < 100; i++) {
-            TransferCkeck transferCkeck = new TransferCkeck();
-            transferCkeck.setBank(bank);
-            transferCkeck.start();
-        }
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(40);
+        executor.execute(() -> {
+            for (int i = 0; i < 10000; i++) {
+                String fromId = String.valueOf((int) (Math.random() * 499));
+                String toId = String.valueOf((int) (Math.random() * 499));
+                long amount = (long) (Math.random() * 51000l);
+                System.out.println("Денег на счету ДО трансфера: " + bank.getAccounts().get(fromId).getMoney() +
+                        System.lineSeparator() + "Количество денег, которые нужно снять со счета " + fromId + ": " + amount);
+                bank.transfer(fromId, toId, amount);
+                System.out.println("Денег на счету ПОСЛЕ трансфера: " + bank.getAccounts().get(fromId).getMoney() +
+                        System.lineSeparator() + "Количество денег, снятых со счета " + fromId + ": " + amount);
+            }
+        });
+
+        executor.shutdown();
     }
 
     private static Account generateAccount(int id) {
