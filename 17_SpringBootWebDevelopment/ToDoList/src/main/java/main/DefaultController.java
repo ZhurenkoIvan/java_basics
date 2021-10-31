8c027b02d0a7f45b63b5ab8f9eaf51c4f2ea6125
Ivan <zhurenko.ivan@mail.ru>
2021-10-31 22:22:02 +0300

    Подредактировал задание 17.1

--- 17_SpringBootWebDevelopment/ToDoList/src/main/java/main/DefaultController.java
+++ 17_SpringBootWebDevelopment/ToDoList/src/main/java/main/DefaultController.java
@@ -1,15 +1,22 @@
 package main;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 @RestController
 public class DefaultController {
 
-    @RequestMapping("/")
-    public String index() {
-        return (new Date().toString());
+    @GetMapping("{toDo}/{count}")
+    public String index(@PathVariable String toDo,@PathVariable int count) {
+        String result = " ";
+        for (int i = 0; i < count; i++) {
+            result.concat(toDo);
+        }
+        return result;
     }
 }
