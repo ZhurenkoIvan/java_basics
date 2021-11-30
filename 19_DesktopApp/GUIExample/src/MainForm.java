d106d47d4f78c140759a288c9400f4973a8e2a31
zhure <Zhurenko.ivan@mail.ru>
2021-11-30 20:49:12 +0300

    Добавил задание 19.4

--- 19_DesktopApp/GUIExample/src/MainForm.java
+++ 19_DesktopApp/GUIExample/src/MainForm.java
@@ -1,30 +1,16 @@
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.beans.PropertyChangeListener;
 
 public class MainForm {
     private JPanel mainPanel;
-    private JButton expandButton;
+    private JTextField nameField;
+    private JTextField secondNameField;
+    private JTextField surnameField;
+    private JLabel nameLabel;
+    private JLabel secondNameLabel;
+    private JLabel surnameLabel;
     private JButton collapseButton;
-    private JPanel expandPanel;
-    private JTextField secondName;
-    private JTextField name;
-    private JTextField surname;
-    private JPanel subExPanel;
-    private JTextArea имяTextArea;
-    private JTextArea фамилияTextArea;
-
-    public MainForm()
-    {
-
-
-    }
 
     public JPanel getMainPanel() {
         return mainPanel;
     }
-
 }
